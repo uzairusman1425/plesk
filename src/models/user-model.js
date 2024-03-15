5af9acc8541a77d11ac3f1fd3c31ca2b8436e41c
Uzair Usman <uzairusman1425@gmail.com>
2024-03-16 03:10:13 +0500

    NextAuth implemented...

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -32,6 +32,6 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.models.users || mongoose.model("users", userSchema)
+const User = mongoose.models.User || mongoose.model("User", userSchema)
 
 export default User
