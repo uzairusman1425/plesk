dc449b9fbea1a86fb59408d2ce548b070770cc6a
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-04-05 01:45:40 +0500

    fkjbds

--- src/app/api/add-employee/route.js
+++ src/app/api/add-employee/route.js
@@ -1,39 +1,74 @@
-import connect from "@/database-config/dbConfig";
-import Customer from "@/models/customer-model";
-import { NextResponse } from "next/server";
+import connect from "@/database-config/dbConfig"
+import Customer from "@/models/customer-model"
+import { NextResponse } from "next/server"
 
 export async function POST(req) {
-	connect();
+	connect()
 	try {
-		const reqBody = await req.json();
-		const { email, phone, professional_details } = reqBody;
+		const reqBody = await req.json()
+		const { email, phone, professional_details } = reqBody
 
 		const employee = await Customer.findOne({
-			$or: [{ email: email }, { "professional_details.user_name": professional_details.user_name }, { "professional_details.email": professional_details.email }, { phone: phone }],
-		});
+			$or: [
+				{ email: email },
+				{
+					"professional_details.user_name":
+						professional_details.user_name
+				},
+				{ "professional_details.email": professional_details.email },
+				{ phone: phone }
+			]
+		})
 
 		if (!reqBody) {
-			return NextResponse.json({ succuss: false, message: "all fileds are required" }, { status: 400 });
+			return NextResponse.json(
+				{ succuss: false, message: "All fields are required!" },
+				{ status: 400 }
+			)
 		}
-		console.log(phone);
+		console.log(phone)
 
 		if (employee) {
 			if (employee.phone === phone) {
-				return NextResponse.json({ succuss: false, message: " phone number already exist  " }, { status: 403 });
-			} else if (employee.professional_details.user_name === professional_details.user_name) {
-				return NextResponse.json({ succuss: false, message: "employ's  already exist with this username " }, { status: 403 });
-			} else if (employee.professional_details.email === professional_details.email) {
-				return NextResponse.json({ succuss: false, message: "employ's professional email already exist   " }, { status: 403 });
+				return NextResponse.json(
+					{ succuss: false, message: "Phone number already exists!" },
+					{ status: 403 }
+				)
+			} else if (
+				employee.professional_details.user_name ===
+				professional_details.user_name
+			) {
+				return NextResponse.json(
+					{ succuss: false, message: "Username already exists!" },
+					{ status: 403 }
+				)
+			} else if (
+				employee.professional_details.email ===
+				professional_details.email
+			) {
+				return NextResponse.json(
+					{
+						succuss: false,
+						message: "Professional email already exist!"
+					},
+					{ status: 403 }
+				)
 			} else if (employee.email === email) {
-				return NextResponse.json({ succuss: false, message: "employ's  email already exist   " }, { status: 403 });
+				return NextResponse.json(
+					{ succuss: false, message: "User email already exists!" },
+					{ status: 403 }
+				)
 			}
 		}
 
-		const Save_employ = new Customer(reqBody);
-		const save = await Save_employ.save();
+		const Save_employ = new Customer(reqBody)
+		const save = await Save_employ.save()
 
-		return NextResponse.json({ succuss: true, data: save }, { status: 200 });
+		return NextResponse.json({ succuss: true, data: save }, { status: 200 })
 	} catch (error) {
-		return NextResponse.json({ succuss: false, message: error.message }, { status: 500 });
+		return NextResponse.json(
+			{ succuss: false, message: error.message },
+			{ status: 500 }
+		)
 	}
 }
