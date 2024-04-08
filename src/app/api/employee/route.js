79c142dd912938c1708e8482c3b5bef7cd28cb74
uzairusman1425 <uzairusman1425@gmail.com>
2024-04-09 02:14:01 +0500

    delete api

--- src/app/api/employee/route.js
+++ src/app/api/employee/route.js
@@ -1,114 +1,97 @@
-import connect from "@/database-config/dbConfig"
-import Customer from "@/models/customer-model"
-import { NextResponse } from "next/server"
+import connect from "@/database-config/dbConfig";
+import Customer, { findOneAndDelete } from "@/models/customer-model";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
-	connect()
+	connect();
 	try {
-		const reqBody = await req.json()
-		const { email, phone, professional_details } = reqBody
+		const reqBody = await req.json();
+		const { email, phone, professional_details } = reqBody;
 
 		const employee = await Customer.findOne({
 			$or: [
 				{ email: email },
 				{
-					"professional_details.user_name":
-						professional_details.user_name
+					"professional_details.user_name": professional_details.user_name,
 				},
 				{ "professional_details.email": professional_details.email },
-				{ phone: phone }
-			]
-		})
+				{ phone: phone },
+			],
+		});
 
 		if (!reqBody) {
-			return NextResponse.json(
-				{ succuss: false, message: "All fields are required!" },
-				{ status: 400 }
-			)
+			return NextResponse.json({ succuss: false, message: "All fields are required!" }, { status: 400 });
 		}
-		console.log(phone)
+		console.log(phone);
 
 		if (employee) {
 			if (employee.phone === phone) {
-				return NextResponse.json(
-					{ succuss: false, message: "Phone number already exists!" },
-					{ status: 403 }
-				)
-			} else if (
-				employee.professional_details.user_name ===
-				professional_details.user_name
-			) {
-				return NextResponse.json(
-					{ succuss: false, message: "Username already exists!" },
-					{ status: 403 }
-				)
-			} else if (
-				employee.professional_details.email ===
-				professional_details.email
-			) {
+				return NextResponse.json({ succuss: false, message: "Phone number already exists!" }, { status: 403 });
+			} else if (employee.professional_details.user_name === professional_details.user_name) {
+				return NextResponse.json({ succuss: false, message: "Username already exists!" }, { status: 403 });
+			} else if (employee.professional_details.email === professional_details.email) {
 				return NextResponse.json(
 					{
 						succuss: false,
-						message: "Professional email already exist!"
+						message: "Professional email already exist!",
 					},
 					{ status: 403 }
-				)
+				);
 			} else if (employee.email === email) {
-				return NextResponse.json(
-					{ succuss: false, message: "User email already exists!" },
-					{ status: 403 }
-				)
+				return NextResponse.json({ succuss: false, message: "User email already exists!" }, { status: 403 });
 			}
 		}
 
-		const Save_employ = new Customer(reqBody)
-		const save = await Save_employ.save()
+		const Save_employ = new Customer(reqBody);
+		const save = await Save_employ.save();
 
-		return NextResponse.json({ succuss: true, data: save }, { status: 200 })
+		return NextResponse.json({ succuss: true, data: save }, { status: 200 });
 	} catch (error) {
-		return NextResponse.json(
-			{ succuss: false, message: error.message },
-			{ status: 500 }
-		)
+		return NextResponse.json({ succuss: false, message: error.message }, { status: 500 });
 	}
 }
 
 export async function GET(req) {
-	connect()
+	connect();
 	try {
-		const id = await req.nextUrl.searchParams.get("id")
+		const id = await req.nextUrl.searchParams.get("id");
 
 		if (id) {
-			const employee = await Customer.findById(id)
+			const employee = await Customer.findById(id);
 			if (!employee) {
-				return NextResponse.json(
-					{ succuss: false, message: "No employee found!" },
-					{ status: 400 }
-				)
+				return NextResponse.json({ succuss: false, message: "No employee found!" }, { status: 400 });
 			}
-			return NextResponse.json(
-				{ succuss: true, data: employee },
-				{ status: 200 }
-			)
+			return NextResponse.json({ succuss: true, data: employee }, { status: 200 });
 		}
 
-		const employee = await Customer.find()
+		const employee = await Customer.find();
 
 		if (!employee) {
-			return NextResponse.json(
-				{ succuss: false, message: "No employee found!" },
-				{ status: 400 }
-			)
+			return NextResponse.json({ succuss: false, message: "No employee found!" }, { status: 400 });
 		}
 
-		return NextResponse.json(
-			{ succuss: true, data: employee },
-			{ status: 200 }
-		)
+		return NextResponse.json({ succuss: true, data: employee }, { status: 200 });
 	} catch (error) {
-		return NextResponse.json(
-			{ succuss: false, message: error.message },
-			{ status: 500 }
-		)
+		return NextResponse.json({ succuss: false, message: error.message }, { status: 500 });
+	}
+}
+
+export async function DELETE(req) {
+	connect();
+	try {
+		const id = await req.nextUrl.searchParams.get("id");
+
+		if (!id) {
+			return NextResponse.json({ success: false, message: "Employee id is null" }, { status: 404 });
+		}
+
+		const employee = await Customer.findOneAndDelete(id);
+		if (!employee) {
+			return NextResponse.json({ success: false, message: "Employee not found" }, { status: 404 });
+		}
+
+		return NextResponse.json({ success: true, message: "Employee deleted successfully" }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ success: false, message: error.message }, { status: 500 });
 	}
 }
