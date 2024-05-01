9ecd19f32c9bdf2ac8de84e9a67476108151da5a
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-05-02 03:51:31 +0500

    Dashboard charts completed...

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -355,9 +355,14 @@ export default function Dashboard() {
 									</AreaChart>
 								</ResponsiveContainer>
 							</div>
-							<p className="text-gray-500">
-								claireracher@onetraveller.co
-							</p>
+							<div className="flex flex-row items-center justify-evenly w-full">
+								<p className="text-gray-500">
+									claireracher@onetraveller.co
+								</p>
+								<p className="py-1 px-3 rounded bg-gray-500 text-white font-semibold text-xs">
+									7h 24m
+								</p>
+							</div>
 						</div>
 						<div className="size-full flex flex-col items-center gap-3">
 							<div className="size-full">
@@ -392,9 +397,14 @@ export default function Dashboard() {
 									</AreaChart>
 								</ResponsiveContainer>
 							</div>
-							<p className="text-gray-500">
-								melissa.morley@onetraveller.co
-							</p>
+							<div className="flex flex-row items-center justify-evenly w-full">
+								<p className="text-gray-500">
+									melissa.morley@onetraveller.co
+								</p>
+								<p className="py-1 px-3 rounded bg-gray-500 text-white font-semibold text-xs">
+									5h 24m
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -419,47 +429,74 @@ export default function Dashboard() {
 						/>
 						<p className="font-medium text-primary">All</p>
 					</div>
-					<div className="h-80 w-full">
-						<ResponsiveContainer width="100%" height="100%">
-							<PieChart width={400} height={400}>
-								<Pie
-									data={data2?.slice(0, 4)}
-									dataKey="hours"
-									cx="50%"
-									cy="50%"
-									outerRadius={115}
-									innerRadius={90}
-									fill="#8884d8"
-								>
-									{data2?.slice(0, 4)?.map((_, key) => (
-										<Cell
+					<div className="flex flex-row gap-5">
+						<div className="size-96">
+							<ResponsiveContainer width="100%" height="100%">
+								<PieChart width={400} height={400}>
+									<Pie
+										data={data2?.slice(0, 4)}
+										dataKey="hours"
+										cx="50%"
+										cy="50%"
+										outerRadius={115}
+										innerRadius={90}
+										fill="#8884d8"
+									>
+										{data2?.slice(0, 4)?.map((_, key) => (
+											<Cell
+												key={key}
+												fill={
+													COLORS[key % COLORS?.length]
+												}
+											/>
+										))}
+									</Pie>
+									<Pie
+										data={data2?.slice(4, 8)}
+										dataKey="hours"
+										cx="50%"
+										cy="50%"
+										innerRadius={115}
+										outerRadius={150}
+										fill="#82ca9d"
+									>
+										{data2?.slice(4, 8)?.map((_, key) => (
+											<Cell
+												key={key}
+												fill={
+													COLORS[
+														(key % COLORS?.length) +
+															4
+													]
+												}
+											/>
+										))}
+									</Pie>
+								</PieChart>
+							</ResponsiveContainer>
+						</div>
+						<div className="flex-1 flex flex-col justify-center gap-3">
+							{COLORS?.map((item, key) => {
+								if (key < data2?.length) {
+									return (
+										<div
+											className="flex flex-row items-center gap-5"
 											key={key}
-											fill={COLORS[key % COLORS?.length]}
-										/>
-									))}
-								</Pie>
-								<Pie
-									data={data2?.slice(4, 8)}
-									dataKey="hours"
-									cx="50%"
-									cy="50%"
-									innerRadius={115}
-									outerRadius={150}
-									fill="#82ca9d"
-								>
-									{data2?.slice(4, 8)?.map((_, key) => (
-										<Cell
-											key={key}
-											fill={
-												COLORS[
-													(key % COLORS?.length) + 4
-												]
-											}
-										/>
-									))}
-								</Pie>
-							</PieChart>
-						</ResponsiveContainer>
+										>
+											<div
+												className="h-[2px] w-8"
+												style={{
+													backgroundColor: item
+												}}
+											/>
+											<p className="text-sm text-gray-500">
+												{data2[key]?.name}
+											</p>
+										</div>
+									)
+								}
+							})}
+						</div>
 					</div>
 				</div>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#39B6E8",
-        secondary: "#0284B8"
-      }
-    }
-  },
-  plugins: [require("tailwind-scrollbar")]
-};
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+	],
+	theme: {
+		screens: {
+			lg: "1250px",
+			xl: "1500px"
+		},
+		extend: {
+			colors: {
+				primary: "#39B6E8",
+				secondary: "#0284B8"
+			}
+		}
+	},
+	plugins: [require("tailwind-scrollbar")]
+}
