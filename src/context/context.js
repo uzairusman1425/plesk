180ce14a5ebdf47cac7dd55cea7a291bc43f0568
techhunt22 <techhunt22@gmail.com>
2024-12-04 23:54:01 +0500

    chnges

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export function useAppContext() {
-	return useContext(AppContext)
+  return useContext(AppContext);
 }
